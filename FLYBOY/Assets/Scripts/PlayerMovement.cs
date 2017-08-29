7fadbe7bc8e60f94963f097c18578a04062aedf2
AIE\s162868 <edwardngo92@gmail.com>
2017-08-29 13:38:21 +1000

    jrtfs

---
+++ FLYBOY/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private bool alive;
+    private GameObject go;
+
+	// Use this for initialization
+	void Start () {
+		go = 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+           transform.position.x -= 10;
+        }
+	}
+}
