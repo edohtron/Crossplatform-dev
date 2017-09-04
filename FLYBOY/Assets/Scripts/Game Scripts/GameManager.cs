7416884d93a76803d6f3423224703c9961a33481
Deeon Burgan <dburgan@live.com>
2017-09-04 11:37:44 +1000

    no message

--- FLYBOY/Assets/Scripts/Game Scripts/GameManager.cs
+++ FLYBOY/Assets/Scripts/Game Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotatePla : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ FLYBOY/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+    private bool alive;
+    private GameObject go;
+    float h;
+    float v;
+    Vector3 direction = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        direction = new Vector3(h, v, 0);
+        direction *= speed * Time.deltaTime;
+
+
+        transform.Translate(direction);
+		//if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(-speed, 0.0f, 0.0f);
+        //}
+        //
+        //if(Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(speed, 0.0f, 0.0f);
+        //}
+        //if(Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(0, speed, 0);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(0, -speed, 0);
+        //}
+    }
+}
