81179e8aef79e349cb1db31199b78abe8ceba9f1
AIE\s162868 <edwardngo92@gmail.com>
2017-09-04 12:09:31 +1000

    no message

---
+++ FLYBOY/Assets/Scripts/Scene Scripts/RevolvePoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevolvePoint : MonoBehaviour {
+
+    public float rotateSpeed = 30;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(Vector3.up * (-rotateSpeed * Time.deltaTime));
+    }
+}
