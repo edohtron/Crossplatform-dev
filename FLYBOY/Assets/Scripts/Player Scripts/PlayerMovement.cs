6e41599f5b2befa01c8bf1ef30a32c131f9bb3e8
Deeon Burgan <dburgan@live.com>
2017-09-05 14:05:36 +1000

    Commit new spaceshit assets

--- FLYBOY/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ FLYBOY/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10.0f;
-    Vector3 MoveUp = new Vector3(0, 10, 0);
-    Vector3 MoveDown = new Vector3(0, -10, 0);
-    Vector3 MoveLeft = new Vector3(-10, 0, 0);
-    Vector3 MoveRight = new Vector3(10, 0, 0);
+    Vector3 MoveUp = new Vector3(0, 1, 0);
+    Vector3 MoveDown = new Vector3(0, -1, 0);
+    Vector3 MoveLeft = new Vector3(-1, 0, 0);
+    Vector3 MoveRight = new Vector3(1, 0, 0);
     // Use this for initialization
     void Start()
     {
@@ -18,24 +18,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.eulerAngles.x >= 350)
+        if (transform.eulerAngles.x >= 300 && transform.eulerAngles.x <= 350)
         {
-           // MoveUp *= speed * Time.deltaTime;
+            //MoveUp *= speed * Time.deltaTime;
             transform.Translate(MoveUp);
         }
-        if(transform.eulerAngles.x <= 15 && transform.eulerAngles.x >= 10)
+        if (transform.eulerAngles.x <= 15 && transform.eulerAngles.x >= 10)
         {
+            //MoveDown *= speed * Time.deltaTime;
             transform.Translate(MoveDown);
         }
-
-        Debug.Log(transform.eulerAngles.y);
-        if (transform.eulerAngles.y >= 300)
+        
+        if (transform.eulerAngles.y >= 300 && transform.eulerAngles.y <= 350)
         {
-            // MoveUp *= speed * Time.deltaTime;
+            //MoveLeft *= speed * Time.deltaTime;
             transform.Translate(MoveLeft);
         }
         if (transform.eulerAngles.y <= 20 && transform.eulerAngles.x >= 10)
         {
+            //MoveRight *= speed * Time.deltaTime;
             transform.Translate(MoveRight);
         }
 
