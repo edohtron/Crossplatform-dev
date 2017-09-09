d84ebee4c5fcef7eac613d11075be69e4fdfc338
ElectroBean <dburgan@live.com>
2017-09-09 18:33:10 +1000

    Added movement bounds and edited scene to make rotation 0

--- FLYBOY/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ FLYBOY/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -12,29 +12,30 @@ public class PlayerMovement : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        Debug.Log("width " + Screen.width);
+        Debug.Log("height " + Screen.height);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.eulerAngles.x >= 300 && transform.eulerAngles.x <= 350)
+        if (transform.eulerAngles.x >= 300 && transform.eulerAngles.x <= 350 && transform.position.y <= (551 + (Screen.width / 7)))
         {
             //MoveUp *= speed * Time.deltaTime;
             transform.Translate(MoveUp);
         }
-        if (transform.eulerAngles.x <= 30 && transform.eulerAngles.x >= 20)
+        if (transform.eulerAngles.x <= 50 && transform.eulerAngles.x >= 0 && transform.position.y >= 564.5)
         {
             //MoveDown *= speed * Time.deltaTime;
             transform.Translate(MoveDown);
         }
         
-        if (transform.eulerAngles.y >= 300 && transform.eulerAngles.y <= 350)
+        if (transform.eulerAngles.y >= 300 && transform.eulerAngles.y <= 350 && transform.position.x >= (0 - (Screen.width/ 15)))
         {
             //MoveLeft *= speed * Time.deltaTime;
             transform.Translate(MoveLeft);
         }
-        if (transform.eulerAngles.y <= 30 && transform.eulerAngles.y >= 20)
+        if (transform.eulerAngles.y <= 50 && transform.eulerAngles.y >= 0 && transform.position.x <= (0 + (Screen.width / 15)))
         {
             //MoveRight *= speed * Time.deltaTime;
             transform.Translate(MoveRight);
